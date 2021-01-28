cf5c7325463d41818edf5a5523de0eec84e47105
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-01-28 19:50:24 +0200

    Complete Main Page

--- FriendsTravelling/src/app/app.module.ts
+++ FriendsTravelling/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './layout/material.module';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeModule } from './features/home/home.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    HomeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ FriendsTravelling/src/app/features/home/home.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { RouterModule } from '@angular/router';
+import { HomeRoutingModule } from './routes/home-routing.module';
+import { MaterialModule } from 'src/app/layout/material.module';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule, RouterModule, MaterialModule],
+})
+export class HomeModule {}

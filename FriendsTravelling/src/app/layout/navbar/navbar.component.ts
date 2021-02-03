886042f432d705fca7f468cc4bb1424735d7dab5
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-02-03 17:57:57 +0200

    Add Drop down menu to navbar

--- FriendsTravelling/src/app/layout/navbar/navbar.component.ts
+++ FriendsTravelling/src/app/layout/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/core/auth';
+import { isNil } from 'lodash';
+import { AuthService, UserInfoService } from 'src/app/core/auth';
+import { AppSettings } from 'src/app/core/settings';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +10,26 @@ import { AuthService } from 'src/app/core/auth';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  constructor(private _authService: AuthService, private router: Router) {}
+  public imagePath: string = '';
 
-  ngOnInit(): void {}
+  constructor(
+    private _authService: AuthService,
+    private router: Router,
+    private _userService: UserInfoService
+  ) {}
+
+  ngOnInit(): void {
+    this._userService.loadUserInfo().subscribe((response) => {
+      this.imagePath = response.profileImagePath || '';
+    });
+  }
+
+  public createImgPath(): string {
+    if (!isNil(this.imagePath) && this.imagePath != '') {
+      return `${AppSettings.hubHost}/${this.imagePath}`;
+    }
+    return '../../../../assets/images/avatar-default.png';
+  }
 
   public OnLogOutButtonCLick(): void {
     this._authService.unauthorize();
